e54df178f198c1a90cb34565e6f377390f999571
kakudasae <kakudasae0131@icloud.com>
2024-12-07 01:19:52 +0000

    あ

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,88 +1,88 @@
 import streamlit as st
 import datetime
 
-# 星座のデータ（ラッキーアイテムを元素記号に変更）
+# 星座のデータ（ラッキーアイテムを理科的なアイテムに変更）
 zodiac_data = {
     'おひつじ座': {
         'dates': [(3, 21), (4, 19)],
-        'lucky_item': 'H（水素）',
+        'lucky_item': '顕微鏡',
         'lucky_color': '赤',
         'compatible_zodiac': ['おうし座', 'しし座', 'いて座'],
         'incompatible_zodiac': ['てんびん座', 'やぎ座']
     },
     'おうし座': {
         'dates': [(4, 20), (5, 20)],
-        'lucky_item': 'O（酸素）',
+        'lucky_item': '化学実験キット',
         'lucky_color': '緑',
         'compatible_zodiac': ['おひつじ座', 'おとめ座', 'やぎ座'],
         'incompatible_zodiac': ['みずがめ座', 'しし座']
     },
     'ふたご座': {
         'dates': [(5, 21), (6, 20)],
-        'lucky_item': 'Fe（鉄）',
+        'lucky_item': '元素記号の板',
         'lucky_color': '黄色',
         'compatible_zodiac': ['てんびん座', 'みずがめ座', 'おひつじ座'],
         'incompatible_zodiac': ['おとめ座', 'さそり座']
     },
     'かに座': {
         'dates': [(6, 21), (7, 22)],
-        'lucky_item': 'Na（ナトリウム）',
+        'lucky_item': 'テストチューブ',
         'lucky_color': '白',
         'compatible_zodiac': ['おうし座', 'おとめ座', 'さそり座'],
         'incompatible_zodiac': ['おひつじ座', 'てんびん座']
     },
     'しし座': {
         'dates': [(7, 23), (8, 22)],
-        'lucky_item': 'Au（金）',
+        'lucky_item': '天体望遠鏡',
         'lucky_color': '金',
         'compatible_zodiac': ['おひつじ座', 'おとめ座', 'いて座'],
         'incompatible_zodiac': ['おうし座', 'さそり座']
     },
     'おとめ座': {
         'dates': [(8, 23), (9, 22)],
-        'lucky_item': 'Cu（銅）',
+        'lucky_item': '物理実験キット',
         'lucky_color': '青',
         'compatible_zodiac': ['おうし座', 'ふたご座', 'やぎ座'],
         'incompatible_zodiac': ['おひつじ座', 'さそり座']
     },
     'てんびん座': {
         'dates': [(9, 23), (10, 22)],
-        'lucky_item': 'N（窒素）',
+        'lucky_item': '化学反応実験セット',
         'lucky_color': 'ピンク',
         'compatible_zodiac': ['ふたご座', 'みずがめ座', 'かに座'],
         'incompatible_zodiac': ['おひつじ座', 'おうし座']
     },
     'さそり座': {
         'dates': [(10, 23), (11, 21)],
-        'lucky_item': 'Cl（塩素）',
+        'lucky_item': '分光器',
         'lucky_color': '黒',
         'compatible_zodiac': ['かに座', 'おとめ座', 'やぎ座'],
         'incompatible_zodiac': ['ふたご座', 'しし座']
     },
     'いて座': {
         'dates': [(11, 22), (12, 21)],
-        'lucky_item': 'Ag（銀）',
+        'lucky_item': '星座モデル',
         'lucky_color': '紫',
         'compatible_zodiac': ['おひつじ座', 'しし座', 'おうし座'],
         'incompatible_zodiac': ['おとめ座', 'てんびん座']
     },
     'やぎ座': {
         'dates': [(12, 22), (1, 19)],
-        'lucky_item': 'Au（金）',
+        'lucky_item': '地球儀',
         'lucky_color': '茶色',
         'compatible_zodiac': ['おうし座', 'おとめ座', 'さそり座'],
         'incompatible_zodiac': ['おひつじ座', 'ふたご座']
     },
     'みずがめ座': {
         'dates': [(1, 20), (2, 18)],
-        'lucky_item': 'He（ヘリウム）',
+        'lucky_item': '水槽',
         'lucky_color': '青',
         'compatible_zodiac': ['ふたご座', 'てんびん座', 'おひつじ座'],
         'incompatible_zodiac': ['おうし座', 'さそり座']
     },
     'うお座': {
         'dates': [(2, 19), (3, 20)],
-        'lucky_item': 'C（炭素）',
+        'lucky_item': '水質測定キット',
         'lucky_color': '水色',
         'compatible_zodiac': ['かに座', 'おとめ座', 'さそり座'],
         'incompatible_zodiac': ['ふたご座', 'いて座']
